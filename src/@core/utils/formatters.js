11629e77bff7e920b30f409e380dd9f2afb9b03e
Ira Nuryani <ira.nuryani@lidahbuayagroup.com>
2025-03-20 10:34:13 +0700

    Make new formatter

--- src/@core/utils/formatters.js
+++ src/@core/utils/formatters.js
@@ -157,5 +157,17 @@ export const getCurrentDateTimeWIB = () => {
 
   return `${year}-${month}-${day}T${hours}:${minutes}`
 }
+export const getCurrentDate = (dateString) => {
+  const date = new Date(dateString)
+
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+
+  return `${year}-${month}-${day}T${hours}:${minutes}`
+}
+
 
 
